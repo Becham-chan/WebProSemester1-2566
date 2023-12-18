f147d0b42ed04301459662a7326e1decc423cb25
SaRai <74460961+aikidoaikido115@users.noreply.github.com>
2023-12-18 21:03:30 +0700

    Add files via upload

---
+++ mongo.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose")
+mongoose.connect("mongodb+srv://admin:1234@cluster1234.miityi3.mongodb.net/?retryWrites=true&w=majority")
+.then(()=>{
+    console.log("mongodb connected");
+})
+.catch(()=>{
+    console.log('failed');
+})
+
+
+const newSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection = mongoose.model("collection",newSchema)
+
+module.exports=collection
